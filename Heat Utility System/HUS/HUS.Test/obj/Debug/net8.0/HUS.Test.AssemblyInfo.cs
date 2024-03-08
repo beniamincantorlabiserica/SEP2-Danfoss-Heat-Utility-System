e52e1a40c2595ba06a05bb4bd9039a068b64a30f
bob <eduards.br7@gmail.com>
2024-03-08 10:43:32 +0100

    ContinuedxUnitSetup

--- Heat Utility System/HUS/HUS.Test/obj/Debug/net8.0/HUS.Test.AssemblyInfo.cs
+++ Heat Utility System/HUS/HUS.Test/obj/Debug/net8.0/HUS.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HUS.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+446b86d0bd95f94c774811c04b6526f4226cd7e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6107be2a59bc00f6c86b727ac59752c90d56e2f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HUS.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HUS.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
