79d47a7aa04a3f65c00c5492cb460b7c1464baab
Alexandru-Daniel Baducu <albad23@student.sdu.dk>
2024-04-21 18:48:15 +0200

    ADDED API requests for updated electricity prices.

---
+++ HUS/Data/PriceRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace HUS.Data;
+
+public class PriceRequest
+{
+    private readonly ApiService _api;
+    
+    public PriceRequest(ApiService? service)
+    {
+        _api = (service is not null) ? service : new ApiService();
+    }
+
+    public float GetElectricityPrice(DateTime date, TimeOnly time)
+    {
+        var res = _api.GetAsync(
+            $"https://api.energidataservice.dk/dataset/DatahubPricelist?start={date.Year}-{date.Month.ToString("00")}-{date.Day.ToString("00")}&end={date.AddDays(1).Year}-{date.AddDays(1).Month.ToString("00")}-{date.AddDays(1).Day.ToString("00")}&limit=1");
+        
+        Rootobject? deserializedObject = JsonSerializer.Deserialize<Rootobject>(res.Result);
+
+        Dictionary<TimeOnly, float> returnDictionary = new Dictionary<TimeOnly, float>();
+
+        returnDictionary.Add(new TimeOnly(00, 00), deserializedObject?.records[0].Price1 ?? default(float));
+        returnDictionary.Add(new TimeOnly(01, 00), deserializedObject?.records[0].Price2 ?? default(float));
+        returnDictionary.Add(new TimeOnly(02, 00), deserializedObject?.records[0].Price3 ?? default(float));
+        returnDictionary.Add(new TimeOnly(03, 00), deserializedObject?.records[0].Price4 ?? default(float));
+        returnDictionary.Add(new TimeOnly(04, 00), deserializedObject?.records[0].Price5 ?? default(float));
+        returnDictionary.Add(new TimeOnly(05, 00), deserializedObject?.records[0].Price6 ?? default(float));
+        returnDictionary.Add(new TimeOnly(06, 00), deserializedObject?.records[0].Price7 ?? default(float));
+        returnDictionary.Add(new TimeOnly(07, 00), deserializedObject?.records[0].Price8 ?? default(float));
+        returnDictionary.Add(new TimeOnly(08, 00), deserializedObject?.records[0].Price9 ?? default(float));
+        returnDictionary.Add(new TimeOnly(09, 00), deserializedObject?.records[0].Price10 ?? default(float));
+        returnDictionary.Add(new TimeOnly(10, 00), deserializedObject?.records[0].Price11 ?? default(float));
+        returnDictionary.Add(new TimeOnly(11, 00), deserializedObject?.records[0].Price12 ?? default(float));
+        returnDictionary.Add(new TimeOnly(12, 00), deserializedObject?.records[0].Price13 ?? default(float));
+        returnDictionary.Add(new TimeOnly(13, 00), deserializedObject?.records[0].Price14 ?? default(float));
+        returnDictionary.Add(new TimeOnly(14, 00), deserializedObject?.records[0].Price15 ?? default(float));
+        returnDictionary.Add(new TimeOnly(15, 00), deserializedObject?.records[0].Price16 ?? default(float));
+        returnDictionary.Add(new TimeOnly(16, 00), deserializedObject?.records[0].Price17 ?? default(float));
+        returnDictionary.Add(new TimeOnly(17, 00), deserializedObject?.records[0].Price18 ?? default(float));
+        returnDictionary.Add(new TimeOnly(18, 00), deserializedObject?.records[0].Price19 ?? default(float));
+        returnDictionary.Add(new TimeOnly(19, 00), deserializedObject?.records[0].Price20 ?? default(float));
+        returnDictionary.Add(new TimeOnly(20, 00), deserializedObject?.records[0].Price21 ?? default(float));
+        returnDictionary.Add(new TimeOnly(21, 00), deserializedObject?.records[0].Price22 ?? default(float));
+        returnDictionary.Add(new TimeOnly(22, 00), deserializedObject?.records[0].Price23 ?? default(float));
+        returnDictionary.Add(new TimeOnly(23, 00), deserializedObject?.records[0].Price24 ?? default(float));
+
+
+        return returnDictionary[time];
+    }
+}
---
+++ HUS/Model/JsonRequestObject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Rootobject
+{
+    public int total { get; set; }
+    public int limit { get; set; }
+    public string dataset { get; set; }
+    public Record[] records { get; set; }
+}
+
+public class Record
+{
+    public string ChargeOwner { get; set; }
+    public string GLN_Number { get; set; }
+    public string ChargeType { get; set; }
+    public string ChargeTypeCode { get; set; }
+    public string Note { get; set; }
+    public string Description { get; set; }
+    public DateTime ValidFrom { get; set; }
+    public DateTime ValidTo { get; set; }
+    public string VATClass { get; set; }
+    public float Price1 { get; set; }
+    public float Price2 { get; set; }
+    public float Price3 { get; set; }
+    public float Price4 { get; set; }
+    public float Price5 { get; set; }
+    public float Price6 { get; set; }
+    public float Price7 { get; set; }
+    public float Price8 { get; set; }
+    public float Price9 { get; set; }
+    public float Price10 { get; set; }
+    public float Price11 { get; set; }
+    public float Price12 { get; set; }
+    public float Price13 { get; set; }
+    public float Price14 { get; set; }
+    public float Price15 { get; set; }
+    public float Price16 { get; set; }
+    public float Price17 { get; set; }
+    public float Price18 { get; set; }
+    public float Price19 { get; set; }
+    public float Price20 { get; set; }
+    public float Price21 { get; set; }
+    public float Price22 { get; set; }
+    public float Price23 { get; set; }
+    public float Price24 { get; set; }
+    public int TransparentInvoicing { get; set; }
+    public int TaxIndicator { get; set; }
+    public string ResolutionDuration { get; set; }
+}
