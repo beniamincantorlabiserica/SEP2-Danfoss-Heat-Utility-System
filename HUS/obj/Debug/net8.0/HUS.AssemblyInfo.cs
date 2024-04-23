8a653ea656b70dbe45e744fa505fd0da3efb5cfe
bob <eduards.br7@gmail.com>
2024-04-23 21:23:01 +0200

    change to sqlite

--- HUS/obj/Debug/net8.0/HUS.AssemblyInfo.cs
+++ HUS/obj/Debug/net8.0/HUS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HUS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734fe91bd80374d9d34a775719c7c5187e0dd375")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8394bd48bec4b5aa4d29939b1a1618e7b143c0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HUS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HUS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
