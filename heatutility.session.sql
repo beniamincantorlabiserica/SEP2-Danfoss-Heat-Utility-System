fdc4b9a118ac4aeef399cc4a650c71dabd5e6702
bob <eduards.br7@gmail.com>
2024-05-16 10:03:41 +0200

    add columns to ProductionUnits

--- heatutility.session.sql
+++ heatutility.session.sql
@@ -1,7 +1,7 @@
 --CREATE TABLE ProductionUnits(
 --    produnit VARCHAR(20) NOT NULL 
---);
-
+ALTER TABLE ProductionUnits 
+ADD ProdGasMotor FLOAT;
 --CREATE TABLE BusinessResultsArchive(result_id INT NOT NULL PRIMARY KEY);
 
 --CREATE TABLE ProductionAccountabilityArchive(result_id INT NOT NULL PRIMARY KEY,cur_demand INT NOT NULL);
