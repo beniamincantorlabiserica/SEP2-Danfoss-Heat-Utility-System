e44ca18e43bf734ecd841127e7c1019beb3afad1
bob <eduards.br7@gmail.com>
2024-05-17 22:03:23 +0200

    add system sqlite and AddData

--- HUS/Model/ResultManager.cs
+++ HUS/Model/ResultManager.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace HUS.Model;
 
@@ -6,6 +9,8 @@ public class ResultManager
 {
     private List<ResultDataPerHour> Results { get; set; }
     
+    private static string connectionString = @"Data source=../HUS/HeatUtilityDB/heatutility.db;Version=3;";
+
     public ResultManager()
     {
         Results = new List<ResultDataPerHour>();
@@ -30,4 +35,44 @@ public class ResultManager
         }
         return totalCost.ToString();
     }
+
+    public static void AddData()
+    {
+        using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+            string hourStart = "12:10";
+            string hourEnd = "12:20";
+            float demand = 12;
+            float electricityPrice = 2;
+            string timePeriod = "1000";
+            float totalCost = 23;
+            double prodGas = 2.234;
+            double prodOil = 1.3;
+            double prodElectric = 0.3;
+            double prodGasMotor = 1.1;
+
+            using (SQLiteCommand command = new SQLiteCommand(connection))
+            {
+                for (int i = 0; i < hourStart.Length; i++)
+                {
+                    command.CommandText = @"INESERT INTO ProductionUnits (HourStart, HourEnd, Demand, ElectricityPrice, TimePeriod, TotalCost, ProdGas, ProdOil, ProdElectric, ProdGasMotor)
+                                        VALUES (@HourStart, @HourEnd, @Demand, @ElectricityPrice, @TimePeriod, @TotalCost, @ProdGas, @ProdOil, @ProdElectric, @ProdGasMotor);";
+                    command.Parameters.AddWithValue("@HourStart", hourStart);
+                    command.Parameters.AddWithValue("@HourEnd", hourEnd);
+                    command.Parameters.AddWithValue("@Demand", demand);
+                    command.Parameters.AddWithValue("@ElectricityPrice", electricityPrice);
+                    command.Parameters.AddWithValue("@TimePeriod", timePeriod);
+                    command.Parameters.AddWithValue("@TotalCost", totalCost);
+                    command.Parameters.AddWithValue("@ProdGas", prodGas);
+                    command.Parameters.AddWithValue("@ProdOil", prodOil);
+                    command.Parameters.AddWithValue("@ProdElectric", prodElectric);
+                    command.Parameters.AddWithValue("@ProdGasMotor", prodGasMotor);
+                    command.ExecuteNonQuery();
+                    command.Parameters.Clear();
+                }
+            }
+        }
+    }
+
 }
--- heatutility.session.sql
+++ heatutility.session.sql
@@ -1,7 +1,14 @@
---CREATE TABLE ProductionUnits(
---    produnit VARCHAR(20) NOT NULL 
-ALTER TABLE ProductionUnits 
-ADD ProdGasMotor FLOAT;
---CREATE TABLE BusinessResultsArchive(result_id INT NOT NULL PRIMARY KEY);
+/*CREATE TABLE ProductionUnits (
+    ProdID INTEGER PRIMARY KEY AUTOINCREMENT,
+    HourStart VARCHAR,
+    HourEnd VARCHAR,
+    Demand FLOAT,
+    ElectricityPrice FLOAT,
+    TimePeriod VARCHAR,
+    TotalCost FLOAT,
+    ProdGas FLOAT,
+    ProdOil FLOAT,
+    ProdElectric FLOAT,
+    ProdGasMotor FLOAT
+);(*/
 
---CREATE TABLE ProductionAccountabilityArchive(result_id INT NOT NULL PRIMARY KEY,cur_demand INT NOT NULL);
